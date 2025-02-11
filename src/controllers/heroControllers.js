f07ed30e9681331981afdd6b3b9385b294e6d6f7
Dede Supriatna <dedesupriatna1421@gmail.com>
2025-02-11 22:29:39 +0700

    feat: add full hero handling

--- src/controllers/heroControllers.js
+++ src/controllers/heroControllers.js
@@ -1,3 +1,6 @@
+import cloudinary from "../config/cloudinary.js";
+import fs from "fs";
+import path from "path";
 import Hero from "../models/heroModel.js";
 
 export const getHero = async (req, res) => {
@@ -12,6 +15,7 @@ export const getHero = async (req, res) => {
 
 export const createHero = async (req, res) => {
   const hero = req.body;
+  const image = req.file;
 
   if (!hero.text) {
     return res
@@ -28,8 +32,23 @@ export const createHero = async (req, res) => {
         .json({ success: false, message: "Data already exists" });
     }
 
+    // Upload image to cloudinary
+    if (image) {
+      const result = await cloudinary.uploader.upload(
+        path.resolve(image.path),
+        {
+          folder: "hero",
+          public_id: "singelton",
+          overwrite: true
+        }
+      );
+      hero.image = result.secure_url;
+      // Delete image from server
+      fs.unlinkSync(image.path);
+    }
+
     // If data doesn't exist in database then create a new data
-    const newHero = new Hero({_id: "singleton", ...hero});
+    const newHero = new Hero({ _id: "singleton", ...hero });
     await newHero.save();
     res.status(201).json({ success: true, data: newHero });
   } catch (error) {
@@ -41,14 +60,32 @@ export const createHero = async (req, res) => {
 export const updateHero = async (req, res) => {
   const id = "singleton";
   const hero = req.body;
+  const image = req.file;
   try {
+    const checkExistingData = await Hero.findById(id);
+    if (!checkExistingData) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Data not found" });
+    }
+
+    if (image) {
+      const result = await cloudinary.uploader.upload(
+        path.resolve(image.path),
+        {
+          folder: "hero",
+          public_id: "singelton",
+          overwrite: true,
+        }
+      );
+      hero.image = result.secure_url;
+      // Delete image from server
+      fs.unlinkSync(image.path);
+    }
+
     const updatedHero = await Hero.findByIdAndUpdate(id, hero, {
       new: true,
     });
-
-    if (!updatedHero) {
-      return res.status(404).json({ success: false, message: "Hero not found" });
-    };
 
     res.status(200).json({ success: true, data: updatedHero });
   } catch (error) {
